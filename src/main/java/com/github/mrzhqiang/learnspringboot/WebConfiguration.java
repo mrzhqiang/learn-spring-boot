d54febfffc71ef13c21060b8990a822ab7c1ace5
张强 <qiang.zhang@touchair.cn>
2019-03-27 13:55:52 +0800

    added: web configuration

---
+++ src/main/java/com/github/mrzhqiang/learnspringboot/WebConfiguration.java
@@ -0,0 +1,10 @@
+package com.github.mrzhqiang.learnspringboot;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author mrzhqiang
+ */
+@Configuration
+public class WebConfiguration {
+}
