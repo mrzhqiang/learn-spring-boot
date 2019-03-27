20f030d18259ecf283edd9fdcf4ac7d8249677ed
张强 <qiang.zhang@touchair.cn>
2019-03-27 13:33:09 +0800

    updated: /user/get api method in user controller

--- src/main/java/com/github/mrzhqiang/learnspringboot/controller/UserController.java
+++ src/main/java/com/github/mrzhqiang/learnspringboot/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.github.mrzhqiang.learnspringboot.controller;
 
+import com.github.mrzhqiang.learnspringboot.model.User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,4 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+  @RequestMapping("/user/get")
+  public User get() {
+    User user = new User();
+    user.username = "v2.1json";
+    user.password = "123$%^";
+    return user;
+  }
 }
