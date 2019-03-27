a4d636f6e99241e9f9fdfb4a3d2f90da88f71975
张强 <qiang.zhang@touchair.cn>
2019-03-27 13:30:23 +0800

    added: user controller

---
+++ src/main/java/com/github/mrzhqiang/learnspringboot/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.github.mrzhqiang.learnspringboot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mrzhqiang
+ */
+@RestController
+public class UserController {
+
+}
