ddff6a817c1c1ea25463e2802fc4942cb503eb2e
张强 <qiang.zhang@touchair.cn>
2019-03-27 13:56:16 +0800

    updated: my filter

--- src/main/java/com/github/mrzhqiang/learnspringboot/WebConfiguration.java
+++ src/main/java/com/github/mrzhqiang/learnspringboot/WebConfiguration.java
@@ -1,5 +1,12 @@
 package com.github.mrzhqiang.learnspringboot;
 
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -7,4 +14,14 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class WebConfiguration {
+  public static class MyFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+      HttpServletRequest servletRequest = (HttpServletRequest) request;
+      System.out.println("This is MyFilter url: " + servletRequest.getRequestURI());
+      chain.doFilter(request, response);
+    }
+  }
 }
