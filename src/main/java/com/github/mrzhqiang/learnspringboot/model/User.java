d9115bb6805fabcea249bb91be3b9c1fd73728c4
张强 <qiang.zhang@touchair.cn>
2019-03-28 09:37:36 +0800

    updated: User

--- src/main/java/com/github/mrzhqiang/learnspringboot/model/User.java
+++ src/main/java/com/github/mrzhqiang/learnspringboot/model/User.java
@@ -1,9 +1,28 @@
 package com.github.mrzhqiang.learnspringboot.model;
 
+import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * @author mrzhqiang
  */
+@Entity
 public class User {
+  @Id
+  @GeneratedValue
+  public Long id;
+  @Column(nullable = false, unique = true)
   public String username;
+  @Column(nullable = false)
   public String password;
+  @Column(nullable = false)
+  public String email;
+  @Column(nullable = false)
+  public String nickname;
+  @Column(nullable = false)
+  public Instant created;
+  public Instant updated;
 }
