81fc790231a367023d6d47baa7c687762bd9634d
张强 <qiang.zhang@touchair.cn>
2019-08-06 14:00:43 +0800

    updated: build setting

--- src/main/java/com/github/mrzhqiang/learnspringboot/LearnSpringBootApplication.java
+++ src/main/java/com/github/mrzhqiang/learnspringboot/LearnSpringBootApplication.java
@@ -2,12 +2,17 @@ package com.github.mrzhqiang.learnspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LearnSpringBootApplication {
+public class LearnSpringBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBootApplication.class, args);
 	}
 
+	@Override protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 }
